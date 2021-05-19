2e80e05e3db268d125835f6350dbd236fc59431e
Jovany Vergara <78619347+Jovany-Vergara@users.noreply.github.com>
2021-05-19 14:03:54 -0500

    Agregando bootstrap dentro de main.js

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import router from './router'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 Vue.use(VueRouter)
 
