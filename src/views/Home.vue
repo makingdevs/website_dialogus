6e87731383b2ba4088ee01e3a4f3e1fe97a0bc7e
Jovany Vergara <78619347+Jovany-Vergara@users.noreply.github.com>
2021-05-27 01:31:33 -0500

    #3 Agregue el fondo con diseño de banner

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,50 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="https://static.wixstatic.com/media/333290_92ba75adb8a84de4a8e36e1c47d29eff~mv2.jpg/v1/fill/w_980,h_716,al_c,q_85,usm_0.66_1.00_0.01/333290_92ba75adb8a84de4a8e36e1c47d29eff~mv2.webp">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="home banner-image w-100 vh-100 d-flex justify-content-center align-items-center">
+      <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
+    <div class="container my-5 d-grid gap-5">
+      <div class="p-5 border">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
+          natus iusto fugit id saepe neque rerum magni laudantium accusantium
+          dolorem numquam quasi.
+        </p>
+      </div>
+      <div class="p-5 border">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
+          natus iusto fugit id saepe neque rerum magni laudantium accusantium
+          dolorem numquam quasi.
+        </p>
+      </div>
+      <div class="p-5 border">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
+          natus iusto fugit id saepe neque rerum magni laudantium accusantium
+          dolorem numquam quasi.
+        </p>
+      </div>
+      <div class="p-5 border">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
+          natus iusto fugit id saepe neque rerum magni laudantium accusantium
+          dolorem numquam quasi.
+        </p>
+      </div>
+      <div class="p-5 border">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Necessitatibus veniam ipsa earum quibusdam, atque ipsum error maiores
+          natus iusto fugit id saepe neque rerum magni laudantium accusantium
+          dolorem numquam quasi. df
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -16,3 +59,9 @@ export default {
   }
 }
 </script>
+<style>
+.banner-image {
+  background-image: url('../assets/fondo1.webp');
+  background-size: cover;
+}
+</style>
