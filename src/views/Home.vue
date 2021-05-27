eabaea05d62c0fe2b3faa54476170db277626518
Jovany Vergara <78619347+Jovany-Vergara@users.noreply.github.com>
2021-05-27 02:08:33 -0500

    #4 agrege el carrusel al html

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,47 @@
   <div>
     <div class="home banner-image w-100 vh-100 d-flex justify-content-center align-items-center">
       <HelloWorld msg="Welcome to Your Vue.js App" />
+      <div id="carouselExampleIndicators"
+        class="carousel slide home banner-image w-100 vh-100 d-flex justify-content-center align-items-center"
+        data-bs-ride="carousel">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+            aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+            aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+            aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item">
+            <img src="../assets/fondo1.webp"
+              class="banner-image w-100 vh-100 d-flex justify-content-center align-items-center"
+              alt="../assets/fondo1.webp">
+          </div>
+          <div class="carousel-item active">
+            <img src="../assets/fondo2.webp"
+              class="banner-image w-100 vh-100 d-flex justify-content-center align-items-center"
+              alt="../assets/fondo2.webp">
+          </div>
+          <div class="carousel-item">
+            <img src="../assets/fondo3.webp"
+              class="banner-image w-100 vh-100 d-flex justify-content-center align-items-center"
+              alt="../assets/fondo3.webp">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
     </div>
+
     <div class="container my-5 d-grid gap-5">
       <div class="p-5 border">
         <p>
@@ -49,19 +89,19 @@
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+  // @ is an alias to /src
+  import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  export default {
+    name: 'Home',
+    components: {
+      HelloWorld
+    }
   }
-}
 </script>
 <style>
-.banner-image {
-  background-image: url('../assets/fondo1.webp');
-  background-size: cover;
-}
+  .banner-image {
+    background-image: url('');
+    background-size: cover;
+  }
 </style>
