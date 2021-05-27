ea1152c0b6e1283d3704b2320bb52e106e630156
Jovany Vergara <78619347+Jovany-Vergara@users.noreply.github.com>
2021-05-27 01:31:03 -0500

    #3 le agregue diseño a la navbar

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,44 @@
 <template>
   <div id="app">
-    <div id="nav">
       <header>
-  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-primary">
-    <a class="navbar-brand home" href="https://www.dialogus.com.mx/"><img src="https://static.wixstatic.com/media/333290_6cf55003a0f3482d95fc20a4530141ea~mv2.png/v1/fill/w_194,h_40,al_c,q_85,usm_0.66_1.00_0.01/dialogusCONTRASTE.webp" alt="dialogusCONTRASTE.png" style="width:194px;height:40px;object-fit:cover;object-position:50% 50%"></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-makingdevs" aria-controls="navbar-makingdevs" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark p-md-3" id="nav">
+      <div class="container">
+        <a class="navbar-brand" href="#"><img src="https://static.wixstatic.com/media/333290_6cf55003a0f3482d95fc20a4530141ea~mv2.png/v1/fill/w_194,h_40,al_c,q_85,usm_0.66_1.00_0.01/dialogusCONTRASTE.webp" alt="dialogusCONTRASTE.png" style="width:194px;height:40px;object-fit:cover;object-position:50% 50%"></a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <div class="collapse navbar-collapse" id="navbar-makingdevs">
-      <ul class="navbar-nav mr-auto">
-      </ul>
-      <div class="my-2 my-md-0">
-        <ul class="navbar-nav mr-auto">
-          
-          <li class="nav-item">
-            <a class="nav-link" href="/training">
-              
-              
-              Dialogüs
-            </a>
-          </li>
-          
-          <li class="nav-item">
-            <a class="nav-link" href="/services">
-              
-              
-              Nosotros
-            </a>
-          </li>
-          
-          <li class="nav-item">
-            <a class="nav-link" href="/#contact-us">
-              
-              
-              servicios
-            </a>
-          </li>
-          
-          <li class="nav-item">
-            <a class="nav-link" href="https://blog.makingdevs.com">
-              
-              
-              Blog
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="/#contact-us">
-              
-              
-              Contacto
-            </a>
-          </li>
-          
-        </ul>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <div class="mx-auto"></div>
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#" id="dialogus">Dialogus | </a> 
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white dialogus" href="#">Nosotros | </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">Servicios | </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white dialogus" href="#">Blog | </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-white" href="#">Contacto</a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </nav>
+    </nav>
 </header>
-    </div>
     <router-view/>
   </div>
 </template>
@@ -73,9 +53,9 @@
 }
 
 #nav {
+  transition: background-color 0.8s linear 0.2s;
   padding: 30px;
 }
-
 #nav a {
   font-weight: bold;
   color: #000000;
@@ -85,3 +65,37 @@
   color: #42b983;
 }
 </style>
+<script>
+
+export default {
+  name: "app",
+  data: function () {
+    return {
+      title: 'La Jungla'
+    };
+  },
+  components: {},
+  created: function () {
+    console.log("Hello word app");
+    window.addEventListener('scroll', function () {
+      if (window.pageYOffset > 100) {
+        this.nav.classList.add('bg-dark', 'shadow');
+      } else {
+        this.nav.classList.remove('bg-dark', 'shadow');
+      }
+    });
+
+  },
+  mounted(){
+    this.$root.$on('title', title => {
+      this.title = title;
+    });
+  },
+  methods: {
+    test(){
+      console.log("test")
+    }
+  },
+};
+</script>
+
