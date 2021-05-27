9ab81cfa67a6f0e35aa840e74e512120b4226b7a
Jovany Vergara <78619347+Jovany-Vergara@users.noreply.github.com>
2021-05-26 19:53:05 -0500

    solo se inserto la imagen

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="https://static.wixstatic.com/media/333290_92ba75adb8a84de4a8e36e1c47d29eff~mv2.jpg/v1/fill/w_980,h_716,al_c,q_85,usm_0.66_1.00_0.01/333290_92ba75adb8a84de4a8e36e1c47d29eff~mv2.webp">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
