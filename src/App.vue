d941f93dd173fb70e92eca649c5b39588e277ad9
Jovany Vergara <78619347+Jovany-Vergara@users.noreply.github.com>
2021-05-28 14:21:02 -0500

    #7 eliminar espacios

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,7 @@
           data-bs-target="#navbarNav"
           aria-controls="navbarNav"
           aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
+          aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
 
