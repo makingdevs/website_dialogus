61d1b25c2e2f62aa01e4be219abec4719e6b8755
Jovany Vergara <78619347+Jovany-Vergara@users.noreply.github.com>
2021-06-08 01:17:29 -0500

    #14 se agrego la path de un ejemplo

--- src/router/index.js
+++ src/router/index.js
@@ -25,7 +25,15 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/TextAdministrations.vue')
-  }
+  },
+  {
+    path: '/themeexample',
+    name: 'ThemeExample',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/ThemeExample.vue')
+  },
 ]
 
 const router = new VueRouter({
