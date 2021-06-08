723a2877dc915271cbfaffc090ede82aa2a82f07
Jovany Vergara <78619347+Jovany-Vergara@users.noreply.github.com>
2021-06-08 01:15:12 -0500

    #14 se importaron los diseños de coach

--- src/main.js
+++ src/main.js
@@ -4,6 +4,30 @@ import VueRouter from 'vue-router'
 import router from './router'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'coach/css/plugins.css'
+import 'coach/search/search.css'
+import 'coach/quform/css/base.css'
+import 'coach/css/styles.css'
+import 'coach/css/custom.css'
+
+import jQuery from 'jquery'
+window.jQuery = jQuery
+
+import 'coach/js/jquery.min.js'
+import 'coach/js/popper.min.js'
+//import 'coach/search/search.js'
+//import 'coach/js/nav-menu.js'
+import './assets/js/search.js'
+import './assets/js/nav-menu.js'
+import 'coach/js/easy.responsive.tabs.js'
+//import 'coach/js/owl.carousel.js'
+import 'coach/js/jquery.counterup.min.js'
+//import 'coach/js/jquery.stellar.min.js'
+import 'coach/js/waypoints.min.js'
+import 'coach/js/countdown.js'
+
+
+
 
 Vue.use(VueRouter)
 import { rtdbPlugin } from 'vuefire'
