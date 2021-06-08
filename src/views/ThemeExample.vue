d54805ef33d5ab8cb44c4cdc577ad8bcf02028d0
Jovany Vergara <78619347+Jovany-Vergara@users.noreply.github.com>
2021-06-08 14:05:24 -0500

    #15 se ajusto el texto a tamaño movil

--- src/views/ThemeExample.vue
+++ src/views/ThemeExample.vue
@@ -1,30 +1,161 @@
 <template>
+  <div>
   <section class="bg-img cover-background full-screen pt-16 pb-8 p-lg-0 top-position min-md-height-auto" data-overlay-dark="0" id="banner">
 
     <div class="container d-flex flex-column">
         <div class="row align-items-center min-lg-vh-100">
             <div class="col-lg-8 mb-5 mb-lg-0">
-                <h6 class="text-primary font-weight-500">gato are in safe hands</h6>
-                <h1 class="text-white display-16 display-md-9 display-lg-7 display-xl-4 mb-1-6 text-shadow font-weight-800">Struggling at some stage in normal life? I can help.</h1>
-                <p class="mb-2-2 w-95 w-md-75 display-29 display-md-28 text-white">The privilege coaching relationship can be an integral asset for proficient development. Shoot for the correct end goal.</p>
-                <a href="#!" class="butn me-2-4 mb-2-4 mb-sm-0"><span>Download Free E-Book</span></a>
+                <h1 class="text-white display-16 display-md-9 display-lg-7 display-xl-4 mb-1-6 text-shadow font-weight-800"> El mejor caso de éxito es cuando existe una transformación</h1>
+                <p class="mb-2-2 w-95 w-md-75 display-29 display-md-28 text-black">En Dialogus nos hemos preocupado y enfocado en el desarrollo del activo más valioso con el que contamos: nuestra gente.</p>
+                <a href="#!" class="butn me-2-4 mb-2-4 mb-sm-0"><span>Descargar guia de Dialogüs</span></a>
                 <div class="d-block d-sm-inline-block">
                     <a class="popup-social-video video_btn small" href="https://www.youtube.com/watch?v=-ESQmzDbnL8"><i class="fas fa-play"></i></a>
-                    <div class="d-inline-block align-middle ps-2-5 text-white text-start font-weight-600">Watch<span class="text-uppercase d-block">intro video</span></div>
+                    <div class="d-inline-block align-middle ps-2-5 text-black text-start font-weight-600">Ver<span class="text-uppercase d-block text-black">Video</span></div>
                 </div>
             </div>
         </div>
     </div>
+  </section>
+ <!-- SERVICE================================================== -->
+
+  <section>
+      <div class="container">
+          <div class="section-heading">
+              <h2 class="h1 text-secondary">¡Cómo puedo ayudarte!</h2>
+              <div class="dotted-box">
+                  <span class="dotted"></span>
+                  <span class="dotted"></span>
+                  <span class="dotted"></span>
+              </div>
+          </div>
+          <div class="row justify-content-center g-0">
+              <div class="col-md-8 col-lg-4">
+                  <div class="service-wrapper">
+                      <div class="service-content">
+                          <img class="image-tamaño" src="../assets/entrena.png" alt="...">
+                          <div class="service-inner">
+                              <div class="content"> 
+                                  <h3>Capital Humano</h3>
+                                  <p class="mb-0">Nuestra división de Capital Humano genera soluciones para la gestión de talento, el desarrollo organizacional y las estrategias</p>
+                                  <div class="service-button">
+                                      <a class="butn btn-white" href="#!"><span>Ver Más</span> <i class="fas fa-arrow-right ms-2"></i></a>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-md-8 col-lg-4">
+                  <div class="service-wrapper">
+                      <div class="service-content">
+                          <img src="../assets/blog2.jpeg" alt="...">
+                          <div class="service-inner">
+                              <div class="content">
+                                  <h3>Estrategia</h3>
+                                  <p class="mb-0">En conjunto con tu empresa, unidad de negocio o área, aplicamos metodologías efectivas e innovadoras para definir el marco de acción.</p>
+                                  <div class="service-button">
+                                      <a class="butn btn-white" href="#!"><span>Ver Más</span> <i class="fas fa-arrow-right ms-2"></i></a>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-md-8 col-lg-4">
+                  <div class="service-wrapper">
+                      <div class="service-content">
+                          <img  class="image-tamaño" src="../assets/curso1.jpeg" alt="...">
+                          <div class="service-inner">
+                              <div class="content">
+                                  <h3>Cumplimiento</h3>
+                                  <p class="mb-0">Desarrollamos sistemas que fomentan la integridad y ética para crear una cultura de honestidad y buenos valores</p>
+                                  <div class="service-button">
+                                      <a class="butn btn-white" href="#!"><span>Ver Más</span> <i class="fas fa-arrow-right ms-2"></i></a>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-md-8 col-lg-4">
+                <div class="service-wrapper">
+                    <div class="service-content">
+                        <img src="../assets/fondo1.webp" alt="...">
+                        <div class="service-inner">
+                            <div class="content">
+                                <h3>Diagnóstico</h3>
+                                <p class="mb-0">Identicamos áreas de oportunidad de las empresas por medio de diversas herramientas de monitoreo y análisis</p>
+                                <div class="service-button">
+                                    <a class="butn btn-white" href="#!"><span>Ver Más</span> <i class="fas fa-arrow-right ms-2"></i></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </div>
+      </div>
 
 </section>
+
+ <!-- ABOUTUS
+        ================================================== -->
+        <section class="bg-light">
+          <div class="container">
+              <div class="row align-items-center">
+                  <div class="col-lg-6 mb-6 mb-md-8 mb-lg-0">
+                      <div class="pe-lg-1-9 pe-xl-2-5 text-center">
+                          <img src="../assets/about-01.jpeg" alt="...">
+                      </div>
+                  </div>
+                  <div class="col-lg-6">
+                      <h2 class="h1 text-black text-secondary mb-1-6 font-weight-700">Certificación internacional de Dialogus ejecutivo!</h2>
+                      <p class="text-black">Este programa le permitirá convertirse en un Entrenador de Líderes y Ejecutivos certificado, lo que ayudará adecuadamente a las asociaciones a dar lo mejor de sí mismas. Este programa Es fundamental para la formación integral de la vida la instrucción, junto con los programas de formación grupal y empresarial.</p>
+                      <ul class="list-style1">
+                          <li  class="text-black"><i class="far fa-arrow-alt-circle-right text-primary me-2"></i>Cursos de Dialogus</li>
+                          <li class="text-black"><i class="far fa-arrow-alt-circle-right text-primary me-2 text-black"></i>Práctica constante y enfoque sistémico</li>
+                          <li class="text-black"><i class="far fa-arrow-alt-circle-right text-primary me-2 text-black"></i>Cuestionario de integración de formación</li>
+                      </ul>
+                  </div>
+                  <div class="col-md-12 ">
+                      <div class="about-content rounded">
+                          <p class="lead text-black">“Generar Prosperidad Incluyente*, mediante el crecimiento y la permanencia de las empresas en el mercado.”</p>
+                          <p class="lead text-black">* Que nos vaya bien a todos.</p>
+                          <div class="text-end">
+                              <img src="../assets/dualo1.webp" alt="...">
+                          </div>
+                      </div>
+                  </div>
+
+              </div>
+          </div>
+      </section>
+
+       <!-- ONLINE COURSES
+        ================================================== -->
+        <section>
+            <div class="container">
+                <div class="section-heading">
+                    <h2 class="h1 text-secondary">Explore Popular Courses</h2>
+                </div>    
+            </div>
+        </section>
+</div>
 </template>
 
 <style>
-
 #banner {
-  background-image: url('../assets/fondo1.webp');
+  background-image: url('../assets/blog1.jpeg');
+ 
 }
-  
+
+.image-tamaño{
+  height: 302px;
+}
+
+.color_34{
+  background-color: rgb(182, 180, 180);
+}
 </style>
 <script>
 
