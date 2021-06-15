3f897381c8afbc0cf905e3696e305f6a13e8a09f
Jovany Vergara <78619347+Jovany-Vergara@users.noreply.github.com>
2021-06-15 12:06:54 -0500

    #21 se agrego el en footer

--- src/App.vue
+++ src/App.vue
@@ -149,7 +149,6 @@
                         <li><a href="blog-details.html">Blog Details</a></li>
                       </ul>
                     </li>
-                    <li><a href="contact.html">Contact</a></li>
                   </ul>
                   <!-- end menu area -->
 
@@ -170,6 +169,143 @@
       </div>
     </header>
     <router-view />
+
+                  <!-- FOOTER
+        ================================================== -->
+        <footer>
+          <div class="container">
+              <div class="row">
+                  <div class="col-sm-6 col-lg-3 mb-2-6 mb-lg-0">
+                      <div class="mb-1-6">
+                          <img src="./assets/logo2.webp" alt="...">
+                      </div>
+                      <p class="mb-1-6 text-white">Brindamos instrucción de vida, junto con el grupo y la empresa también.</p>
+                      <div class="row align-items-center">
+                          <div class="col-lg-12 mb-3">
+                              <div class="media">
+                                  <div class="address-icon">
+                                      <i class="far fa-envelope"></i>
+                                  </div>
+                                  <div class="media-body align-self-center">
+                                      <h6 class="text-white mb-0">Email </h6>
+                                      <p class="text-white mb-0">contactanos@dialogus.com.mx</p>
+                                  </div>
+                              </div>
+                          </div>
+                          <div class="col-lg-12 mb-3">
+                              <div class="media">
+                                  <div class="address-icon">
+                                      <i class="fas fa-mobile-alt"></i>
+                                  </div>
+                                  <div class="media-body align-self-center">
+                                      <h6 class="text-white mb-0">Whatsapp</h6>
+                                      <p class="text-white mb-0">(+44) 123 456 789</p>
+                                  </div>
+                              </div>
+                          </div>
+                          <div class="col-lg-12">
+                              <div class="media">
+                                  <div class="address-icon">
+                                      <i class="fas fa-map-marker-alt"></i>
+                                  </div>
+                                  <div class="media-body align-self-center">
+                                      <h6 class="text-white mb-0">Localización</h6>
+                                      <p class="text-white mb-0">66 Guild Street 512B, Great North Town.</p>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-sm-6 col-lg-3 mb-2-6 mb-lg-0">
+                      <div class="ps-md-1-6 ps-xl-1-9">
+                          <h3 class="footer-title h5">Explorar</h3>
+                          <ul class="list-style3">
+                              <li><a href="#!">About</a></li>
+                              <li><a href="#!">Portfolio</a></li>
+                              <li><a href="#!">Courses</a></li>
+                              <li><a href="#!">FAQ</a></li>
+                              <li><a href="#!">Blog</a></li>
+                              <li><a href="#!">Contact</a></li>
+                          </ul>
+                      </div>
+                  </div>
+                  <div class="col-sm-6 col-lg-3 mb-2-6 mb-sm-0">
+                      <div class="ps-lg-1-6 ps-xl-1-9">
+                          <h3 class="footer-title h5">Experiencia</h3>
+                          <ul class="list-style3">
+                              <li><a href="#!">Coaching</a></li>
+                              <li><a href="#!">Certification</a></li>
+                              <li><a href="#!">Consulting</a></li>
+                              <li><a href="#!">Membership</a></li>
+                              <li><a href="#!">Online Courses</a></li>
+                              <li><a href="#!">Events</a></li>
+                          </ul>
+                      </div>
+                  </div>
+                  <div class="col-sm-6 col-lg-3">
+                      <div class="ps-md-1-6 ps-xl-1-9">
+                          <h3 class="footer-title h5">Boletin informativo</h3>
+                          <p class="text-white">Manténgase actualizado con nuestras últimas noticias. ¡Prometemos no hacer spam!</p>
+                          <form class="quform newsletter-form w-90 w-sm-100" action="quform/newsletter-two.php" method="post" enctype="multipart/form-data" onclick="">
+
+                              <div class="quform-elements">
+
+                                  <div class="row">
+
+                                      <!-- Begin Text input element -->
+                                      <div class="col-md-12">
+                                          <div class="quform-element">
+                                              <div class="quform-input">
+                                                  <input class="form-control" id="email_address" type="text" name="email_address" placeholder="Subscribe with us" />
+                                              </div>
+                                          </div>
+                                      </div>
+                                      <!-- End Text input element -->
+
+                                      <!-- Begin Submit button -->
+                                      <div class="col-md-12">
+                                          <div class="quform-submit-inner">
+                                              <button class="butn m-0" type="submit"><span>Subscribete <i class="fas fa-arrow-right ms-2"></i></span></button>
+                                          </div>
+                                          <div class="quform-loading-wrap text-start"><span class="quform-loading"></span></div>
+                                      </div>
+                                      <!-- End Submit button -->
+
+                                  </div>
+
+                              </div>
+
+                          </form>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div class="footer-bar">
+              <div class="container">
+                  <div class="row">
+                      <div class="col-md-6 text-center text-md-start mt-3 mt-md-0 order-2 order-md-1">
+                          <p class="d-inline-block text-white mb-0">&copy; 2021 Desarrollado por <a href="https://www.dialogus.com.mx/" class="text-secondary">Dialogüs</a></p>
+                      </div>
+                      <div class="col-md-6 text-md-end order-1 order-md-2">
+                          <p class="text-white d-inline-block font-weight-600 mb-0 align-middle">Estamos en: </p>
+                          <ul class="share-post">
+                              <li>
+                                  <a href="https://www.facebook.com/DialogusConsultores"><i class="fab fa-facebook-f"></i></a>
+                              </li>
+                              <li>
+                                  <a href="https://twitter.com/search?q=%40Dialogus_LAT&src=typed_query"><i class="fab fa-twitter"></i></a>
+                              </li>
+                              <li>
+                                  <a href="https://mx.linkedin.com/jobs/search?keywords=Dialogus-consultores&location=Ecatepec%20de%20Morelos%2C%20M%C3%A9xico%2C%20M%C3%A9xico&geoId=101340577&trk=public_jobs_jobs-search-bar_search-submit&position=1&pageNum=0"><i class="fab fa-linkedin-in"></i></a>
+                              </li>
+                          </ul>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </footer>
+
+
   </div>
 </template>
 
