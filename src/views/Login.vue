b5653521232c01f6497da814e369fc46009ffbc5
Brandon Vergara <brandon@makingdevs.com>
2021-06-16 15:03:46 -0500

    [23] removing elements not used

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,16 +3,12 @@
 		<form @submit="checkLogin">
 			<div class="mb-3">
 				<label for="exampleInputuser1" class="form-label">user address</label>
-				<input type="user" class="form-control" id="exampleInputuser1" aria-describedby="userHelp" v-model="user.username">
-				<div id="userHelp" class="form-text">We'll never share your user with anyone else.</div>
+				<input type="user" class="form-control" v-model="user.username">
+				<div class="form-text">We'll never share your user with anyone else.</div>
 			</div>
 			<div class="mb-3">
 				<label for="exampleInputPassword1" class="form-label">Password</label>
 				<input type="password" class="form-control" id="exampleInputPassword1" v-model="user.pass">
-			</div>
-			<div class="mb-3 form-check">
-				<input type="checkbox" class="form-check-input" id="exampleCheck1">
-				<label class="form-check-label" for="exampleCheck1">Check me out</label>
 			</div>
 			<button type="submit" class="btn btn-primary">Submit</button>
 		</form>
