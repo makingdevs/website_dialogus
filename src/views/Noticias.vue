906bd4a2aa5328fd5806104c3c681b4856ae3a3b
Jovany Vergara <78619347+Jovany-Vergara@users.noreply.github.com>
2021-06-21 16:02:16 -0500

    #35 se agrego el contenido

--- src/views/Noticias.vue
+++ src/views/Noticias.vue
@@ -3,7 +3,7 @@
         
         <!-- PAGE TITLE
         ================================================== -->
-        <section class="page-title-section top-position bg-img cover-background" data-overlay-dark="55" id="banner2">
+        <section class="parallax page-title-section top-position bg-img cover-background" data-overlay-dark="55" id="banner2">
             <div class="container">
 
                 <div class="row">
@@ -33,7 +33,7 @@
         <section class="blog">
             <div class="container">
                 <div class="section-heading">
-                    <h2 class="h1 text-secondary">Our Latest Blog</h2>
+                    <h2 class="h1 text-secondary">Nuestro Blog</h2>
                     <div class="dotted-box">
                         <span class="dotted"></span>
                         <span class="dotted"></span>
@@ -44,66 +44,127 @@
                     <div class="col-lg-6 col-xl-4 mb-1-6 mb-md-1-9">
                         <article class="card card-style2 h-100">
                             <div class="card-img">
-                                <img src="../assets/persona1.webp" alt="...">
+                                <img src="../assets/persona2.webp" alt="...">
                             </div>
                             <div class="card-body">
                                 <ul class="meta-tags">
                                     <li>
-                                            <i class="ti-calendar me-1 text-primary"></i> Feb 18, 2021
+                                        <router-link to="/noticiasDetalles" class="">
+                                            <i class="ti-calendar me-1 text-warning"></i> Feb 19, 2021
+                                        </router-link>
                                     </li>
                                 </ul>
-                                <h3 class="h4 mb-3"><a href="blog-details.html">5 Facts about life coach that will make you think twice.</a></h3>
-                                <p>The purpose of the masterclass is to introduce the key thoughts supporting the psychodynamic approach.</p>
-                                <router-link to="#" class=""></router-link>
-                                <a href="blog-details.html" class="font-weight-600">read more...</a>
+                                <h3 class="h4 mb-3">5 Facts about life coach that will make you think twice.</h3>
+                                <p  align="left" class="text-dark">The purpose of the masterclass is to introduce the key thoughts supporting the psychodynamic approach.</p>
+                                <router-link to="/noticiasDetalles" class="font-weight-600"> Ver Más</router-link>
+
+                            </div>
+                        </article>
+                    </div>
+                    <div class="col-lg-6 col-xl-4 mb-1-6 mb-md-1-9 ">
+                        <article class="card card-style2 h-100 ">
+                            <div class="card-img">
+                                <img src="../assets/persona4.webp" alt="">
+                            </div>
+                            <div class="card-body">
+                                <ul class="meta-tags">
+                                    <li>
+                                        <router-link to="/noticiasDetalles" class="">
+                                            <i class="ti-calendar me-1 text-warning"></i> Feb 09, 2021
+                                        </router-link>
+                                    </li>
+                                </ul>
+                                <h3 class="h4 mb-3">20 Wonderful life coach. Number 16 is absolutely stunning!</h3>
+                                <p align="left" class="text-dark">The purpose of the masterclass is to introduce the key thoughts supporting the psychodynamic approach.</p>
+                                <router-link to="/noticiasDetalles" class="font-weight-600">Ver Más</router-link>
                             </div>
                         </article>
                     </div>
                     <div class="col-lg-6 col-xl-4 mb-1-6 mb-md-1-9">
                         <article class="card card-style2 h-100">
                             <div class="card-img">
-                                <img src="img/blog/blog-02.jpg" alt="...">
+                                <img src="../assets/persona3.webp" alt="...">
                             </div>
                             <div class="card-body">
                                 <ul class="meta-tags">
                                     <li>
                                         <a href="#!">
-                                            <i class="ti-calendar me-1 text-primary"></i> Feb 09, 2021
+                                            <i class="ti-calendar me-1 text-warning"></i> Jan 24, 2021
                                         </a>
                                     </li>
+
+                                </ul>
+                                <h3 class="h4 mb-3"><router-link to="">How to have a fantastic life coach with minimal spending?</router-link></h3>
+                                <p  align="left" class="text-dark">The purpose of the masterclass is to introduce the key thoughts supporting the psychodynamic approach.</p>
+                                <router-link to="blog-details.html" class="font-weight-600">Ver Más</router-link>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="section-heading">
+                        <h2 class="h1 text-secondary">Nuestros Eventos</h2>
+                        <div class="dotted-box">
+                            <span class="dotted"></span>
+                            <span class="dotted"></span>
+                            <span class="dotted"></span>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-xl-4 mb-1-6 mb-md-1-9">
+                        <article class="card card-style2 h-100">
+                            <div class="card-img">
+                                <img src="../assets/persona5.webp" alt="...">
+                            </div>
+                            <div class="card-body">
+                                <ul class="meta-tags">
                                     <li>
                                         <a href="#!">
-                                            <i class="ti-comment me-1 text-primary"></i> 12 Comment
+                                            <i class="ti-calendar me-1 text-warning"></i> Jan 24, 2021
                                         </a>
                                     </li>
+
                                 </ul>
-                                <h3 class="h4 mb-3"><a href="blog-details.html">20 Wonderful life coach. Number 16 is absolutely stunning!</a></h3>
-                                <p>The purpose of the masterclass is to introduce the key thoughts supporting the psychodynamic approach.</p>
-                                <a href="blog-details.html" class="font-weight-600">read more...</a>
+                                <h3 class="h4 mb-3"><router-link to="">How to have a fantastic life coach with minimal spending?</router-link></h3>
+                                <p  align="left" class="text-dark">The purpose of the masterclass is to introduce the key thoughts supporting the psychodynamic approach.</p>
+                                <router-link to="blog-details.html" class="font-weight-600">Ver Más</router-link>
                             </div>
                         </article>
                     </div>
                     <div class="col-lg-6 col-xl-4 mb-1-6 mb-md-1-9">
                         <article class="card card-style2 h-100">
                             <div class="card-img">
-                                <img src="img/blog/blog-03.jpg" alt="...">
+                                <img src="../assets/persona2.webp" alt="...">
                             </div>
                             <div class="card-body">
                                 <ul class="meta-tags">
                                     <li>
                                         <a href="#!">
-                                            <i class="ti-calendar me-1 text-primary"></i> Jan 24, 2021
+                                            <i class="ti-calendar me-1 text-warning"></i> Jan 24, 2021
                                         </a>
                                     </li>
+
+                                </ul>
+                                <h3 class="h4 mb-3"><router-link to="">How to have a fantastic life coach with minimal spending?</router-link></h3>
+                                <p  align="left" class="text-dark">The purpose of the masterclass is to introduce the key thoughts supporting the psychodynamic approach.</p>
+                                <router-link to="blog-details.html" class="font-weight-600">Ver Más</router-link>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="col-lg-6 col-xl-4 mb-1-6 mb-md-1-9">
+                        <article class="card card-style2 h-100">
+                            <div class="card-img">
+                                <img src="../assets/persona4.webp" alt="...">
+                            </div>
+                            <div class="card-body">
+                                <ul class="meta-tags">
                                     <li>
                                         <a href="#!">
-                                            <i class="ti-comment me-1 text-primary"></i> 24 Comment
+                                            <i class="ti-calendar me-1 text-warning"></i> Jan 24, 2021
                                         </a>
                                     </li>
+
                                 </ul>
-                                <h3 class="h4 mb-3"><a href="blog-details.html">How to have a fantastic life coach with minimal spending?</a></h3>
-                                <p>The purpose of the masterclass is to introduce the key thoughts supporting the psychodynamic approach.</p>
-                                <a href="blog-details.html" class="font-weight-600">read more...</a>
+                                <h3 class="h4 mb-3"><router-link to="">How to have a fantastic life coach with minimal spending?</router-link></h3>
+                                <p  align="left" class="text-dark">The purpose of the masterclass is to introduce the key thoughts supporting the psychodynamic approach.</p>
+                                <router-link to="blog-details.html" class="font-weight-600">Ver Más</router-link>
                             </div>
                         </article>
                     </div>
@@ -111,15 +172,15 @@
                 </div>
                 <div class="row">
                     <div class="col-sm-12">
+
                         <!-- start pager  -->
                         <div class="text-center mt-6 mt-lg-7">
                             <div class="pagination">
                                 <ul>
-                                    <li><a href="#!" class="me-3"><i class="fas fa-long-arrow-alt-left"></i></a></li>
+                                    <li><router-link to="#!" class="me-3"><i class="fas fa-long-arrow-alt-left"></i></router-link></li>
                                     <li class="active"><a href="#!" class="me-2">1</a></li>
-                                    <li><a href="#!" class="me-2">2</a></li>
-                                    <li><a href="#!" class="me-3">3</a></li>
-                                    <li><a href="#!"><i class="fas fa-long-arrow-alt-right"></i></a></li>
+
+                                    <li><router-link to="#!"><i class="fas fa-long-arrow-alt-right"></i></router-link></li>
                                 </ul>
                             </div>
                         </div>
@@ -135,4 +196,15 @@
     #banner2 {
         background-image: url('../assets/fondo5.jpeg');
     }
+.tamañoCuadros{
+    object-fit: cover;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    overflow: hidden;
+    border-radius: 0;
+}
 </style>
