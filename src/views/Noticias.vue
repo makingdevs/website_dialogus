4b463c15f78e0275f093c3527a50396b3aadc5db
Jovany Vergara <78619347+Jovany-Vergara@users.noreply.github.com>
2021-06-21 11:18:59 -0500

    #34 se agrego el apartado de noticias en views

---
+++ src/views/Noticias.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        
+        <!-- PAGE TITLE
+        ================================================== -->
+        <section class="page-title-section top-position bg-img cover-background" data-overlay-dark="55" id="banner2">
+            <div class="container">
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1>Noticias</h1>
+                    </div>
+                    <div class="col-md-12">
+                        <ul class="ps-0">
+                            <li>
+                                <router-link to="/home" class="">
+                                    Home
+                                  </router-link></li>
+                            <li>
+                                <router-link to="/noticias" class="">
+                                    Noticias
+                                </router-link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    </div>
+</template>
+
+<style>
+    #banner2 {
+        background-image: url('../assets/fondo5.jpeg');
+    }
+</style>
