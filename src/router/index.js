ef3f2afc8d2269c43190cc5eca936008316cc363
Jovany Vergara <78619347+Jovany-Vergara@users.noreply.github.com>
2021-06-21 12:09:39 -0500

    #35 se agrego el home bien

--- src/router/index.js
+++ src/router/index.js
@@ -7,9 +7,17 @@ Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/home',
+    path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Home.vue')
   },
   {
     path: '/about',
