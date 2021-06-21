bdda4fb4884e8e9c8dd0e18260f8a70fd5b77f68
Jovany Vergara <78619347+Jovany-Vergara@users.noreply.github.com>
2021-06-21 11:17:47 -0500

    #34 se creo el link de Noticias

--- src/router/index.js
+++ src/router/index.js
@@ -46,6 +46,14 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
   },
+  {
+    path: '/noticias',
+    name: 'Noticias',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Noticias.vue')
+  },
 ]
 
 const router = new VueRouter({
