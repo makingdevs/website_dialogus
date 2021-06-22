f78579cf5f251d2afd0846f33305dadcfbd1a394
Octavio Lugo <octavio@makingdevs.com>
2021-06-21 20:44:41 -0500

    #33 Bosquejo Nosotros

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,15 +1,342 @@
 <template>
   <div class="about">
-    <section class="page-title-section top-position bg-img cover-background" data-overlay-dark="55" data-background="../assets/blog2.jpeg">
+        <!-- PAGE TITLE
+        ================================================== -->
+        <section class="page-title-section top-position bg-img cover-background" id="bannerabout" data-overlay-dark="55" data-background="img/bg/bg-04.jpg">
             <div class="container">
 
                 <div class="row">
                     <div class="col-md-12">
                         <h1>Nosotros</h1>
                     </div>
+                    <div class="col-md-12">
+                        <ul class="ps-0">
+                            <li><a href="/home">Home</a></li>
+                            <li><a href="#!">Nosotros</a></li>
+                        </ul>
+                    </div>
                 </div>
 
             </div>
         </section>
+
+       
+       <!-- Misión Visión
+        ================================================== -->
+    <section class="bg-light">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-6 mb-6 mb-md-8 mb-lg-0">
+            <div class="pe-lg-1-9 pe-xl-2-5 text-center">
+              <img class="box" src="../assets/persona5.webp" alt="...">
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <h2 class="h1 text-black text-secondary mb-1-6 font-weight-700">Nuestra Misión</h2>
+            <p class="text-black">Acompañamos el desarrollo de nuestros clientes, mediante metodologías innovadoras contribuyendo en su rentabilidad, productividad y satisfacción.</p>
+            <h2 class="h1 text-black text-secondary mb-1-6 font-weight-700">Nuestra Visión</h2>
+            <p class="text-black">Generar Prosperidad Incluyente*, mediante el crecimiento y la permanencia de las empresas en el mercado. *Que nos vaya bien a todos.</p>
+            
+            <ul class="list-style1">
+            </ul>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- COUNTER
+        ================================================== -->
+        <section class="py-0">
+            <div class="row g-0">
+                <div class="col-lg-6 bg-img cover-background min-height-300" data-overlay-dark="2" data-background="../assets/img4.jpg" id="conteo">
+                    <div class="d-table h-100 text-center mx-auto position-relative z-index-1">
+                        <div class="d-table-cell vertical-align-middle h-100 ">
+                            <div class="d-inline-block vertical-align-middle z-index-1 text-start">
+                                <a class="popup-social-video video_btn" href="https://www.youtube.com/watch?v=-ESQmzDbnL8"><i class="fas fa-play"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 bg-secondary">
+                    <div class="p-2-5 p-sm-2-7 p-md-6 p-lg-9 p-xl-14">
+                        <div class="row g-0 justify-content-center">
+                            <div class="col-sm-6 text-center border-sm-end border-bottom border-white-opacity2 py-1-6">
+                                <span class="countup text-white h1 font-weight-700">45</span>
+                                <p class="display-28 display-xl-27 text-white font-weight-500 m-0">Cursos</p>
+                            </div>
+                            <div class="col-sm-6 text-center border-bottom border-white-opacity2 py-1-6">
+                                <span class="countup text-white h1 font-weight-700">1800</span>
+                                <p class="display-28 display-xl-27 text-white font-weight-500 m-0">Clientes felices</p>
+                            </div>
+                            <div class="col-sm-6 text-center borders-bottom border-sm-bottom-0 border-sm-end border-white-opacity2 py-1-6">
+                                <span class="countup text-white h1 font-weight-700">1500</span>
+                                <p class="display-28 display-xl-27 text-white font-weight-500 m-0">Satisfacción</p>
+                            </div>
+                            <div class="col-sm-6 text-center py-1-6">
+                                <span class="countup text-white h1 font-weight-700">21</span>
+                                <p class="display-28 display-xl-27 text-white font-weight-500 m-0">Premios ganados</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    <!-- Clientes
+        ================================================== -->
+        <section class="bg-light">
+            <div class="container">
+                <div class="section-heading">
+                    <h2 class="h1 text-secondary">Nuestros Clientes</h2>
+                    <div class="dotted-box">
+                        <span class="dotted"></span>
+                        <span class="dotted"></span>
+                        <span class="dotted"></span>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-6 col-lg-4 mb-1-9 mb-lg-0">
+                        <div class="team-wrapper">
+                            <div class="img-holder">
+                                <div class="team-wrapper-inner">
+                                    <img src="../assets/persona2.webp" alt="...">
+                                    <div class="team-social-links">
+                                        <ul class="social-icons">
+                                            <li>
+                                                <a href="#!"><i class="fab fa-facebook-f"></i></a>
+                                            </li>
+                                            <li>
+                                                <a href="#!"><i class="fab fa-twitter"></i></a>
+                                            </li>
+                                            <li>
+                                                <a href="#!"><i class="fab fa-linkedin-in"></i></a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="team-content">
+                                    <h4 class="h5"><a href="#!">Cliente 1</a></h4>
+                                    <p class="text-primary font-weight-600 mb-0">Empresa X</p>
+                                    <i class="fas fa-share-alt pink"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-4 mb-1-9 mb-lg-0">
+                        <div class="team-wrapper">
+                            <div class="img-holder">
+                                <div class="team-wrapper-inner">
+                                    <img src="../assets/persona3.webp" alt="...">
+                                    <div class="team-social-links">
+                                        <ul class="social-icons">
+                                            <li>
+                                                <a href="#!"><i class="fab fa-facebook-f"></i></a>
+                                            </li>
+                                            <li>
+                                                <a href="#!"><i class="fab fa-twitter"></i></a>
+                                            </li>
+                                            <li>
+                                                <a href="#!"><i class="fab fa-linkedin-in"></i></a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="team-content">
+                                    <h4 class="h5"><a href="#!">Cliente 2</a></h4>
+                                    <p class="text-primary font-weight-600 mb-0">Empresa Y</p>
+                                    <i class="fas fa-share-alt pink"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6 col-lg-4">
+                        <div class="team-wrapper">
+                            <div class="img-holder">
+                                <div class="team-wrapper-inner">
+                                    <img src="../assets/persona4.webp" alt="...">
+                                    <div class="team-social-links">
+                                        <ul class="social-icons">
+                                            <li>
+                                                <a href="#!"><i class="fab fa-facebook-f"></i></a>
+                                            </li>
+                                            <li>
+                                                <a href="#!"><i class="fab fa-twitter"></i></a>
+                                            </li>
+                                            <li>
+                                                <a href="#!"><i class="fab fa-linkedin-in"></i></a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="team-content">
+                                    <h4 class="h5"><a href="#!">Cliente 3</a></h4>
+                                    <p class="text-primary font-weight-600 mb-0">Empresa Z</p>
+                                    <i class="fas fa-share-alt pink"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+         
+         <!-- CARROUSEL -->
+        <section class="position-relative z-index-9">
+        <div class="parallax bg-img section-bg" data-overlay-dark="4" data-background="img/bg/bg-02.jpg" style="background-image: url(&quot;img/bg/bg-02.jpg&quot;);"></div>
+        <div class="container">
+            <div class="section-heading">
+            <h2 class="h1 text-white">Nuestros CLientes</h2>
+            </div>
+            <div class="testimonial-carousel owl-carousel owl-theme owl-loaded owl-drag">
+            <div class="owl-stage-outer">
+                <div class="owl-stage" style="transform: translate3d(-2940px, 0px, 0px); transition: all 1.5s ease 0s; width: 4704px;">
+                <div class="owl-item cloned" style="width: 558px; margin-right: 30px;">
+                    <div class="testimonial-wrapper">
+                    <div class="testimonial-author d-flex justify-content-center">
+                        <img src="img/avatar/avatar-09.jpg" alt="...">
+                    </div>
+                    <div class="testimonial-text">
+                        <p class="font-weight-500 display-29 display-lg-28">Thanks to Life coach, After using Life coach my business skyrocketed! It really saves me time and effort. Life coach is exactly what our business has been lacking.</p>
+                    </div>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="author-details">
+                        <h4 class="h6 text-primary">Rock James</h4>
+                        <div class="rating">
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                        </div>
+                        <p class="mb-0">Internal Revenue Service Agent</p>
+                        </div>
+                        <div class="testimonial-quote">
+                        <i class="fas fa-quote-right"></i>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div class="owl-item cloned" style="width: 558px; margin-right: 30px;">
+                    <div class="testimonial-wrapper">
+                    <div class="testimonial-author d-flex justify-content-center">
+                        <img src="img/avatar/avatar-10.jpg" alt="...">
+                    </div>
+                    <div class="testimonial-text">
+                        <p class="font-weight-500 display-29 display-lg-28">Needless to say we are extremely satisfied with the results. Very easy to use. It really saves me time and effort. Life coach is exactly what our business has been lacking.</p>
+                    </div>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="author-details">
+                        <h4 class="h6 text-primary">Gussy Sarah</h4>
+                        <div class="rating">
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                        </div>
+                        <p class="mb-0">Smokehouse Worker</p>
+                        </div>
+                        <div class="testimonial-quote">
+                        <i class="fas fa-quote-right"></i>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div class="owl-item" style="width: 558px; margin-right: 30px;">
+                    <div class="testimonial-wrapper">
+                    <div class="testimonial-author d-flex justify-content-center">
+                        <img src="img/avatar/avatar-07.jpg" alt="...">
+                    </div>
+                    <div class="testimonial-text">
+                        <p class="font-weight-500 display-29 display-lg-28">I like Life coach more and more each day because it makes my life a lot easier. Thanks for the great service. Since I invested in Life coach I made over 100,000 dollars profits.</p>
+                    </div>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="author-details">
+                        <h4 class="h6 text-primary">Shurwood Smith</h4>
+                        <div class="rating">
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                        </div>
+                        <p class="mb-0">Museum Exhibit Technician</p>
+                        </div>
+                        <div class="testimonial-quote">
+                            <i class="fas fa-quote-right"></i>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div class="owl-item" style="width: 558px; margin-right: 30px;">
+                    <div class="testimonial-wrapper">
+                    <div class="testimonial-author d-flex justify-content-center">
+                        <img src="img/avatar/avatar-08.jpg" alt="...">
+                    </div>
+                    <div class="testimonial-text">
+                        <p class="font-weight-500 display-29 display-lg-28">Thanks for the great service. You won't regret it. I use Life coach often. Life coach is the most valuable business resource we have EVER purchased courses. I use Life coach often.</p>
+                    </div>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="author-details">
+                        <h4 class="h6 text-primary">Kyla Turner</h4>
+                        <div class="rating">
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                        </div>
+                        <p class="mb-0">Electronics Warfare</p>
+                        </div>
+                        <div class="testimonial-quote">
+                        <i class="fas fa-quote-right"></i>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div class="owl-item" style="width: 558px; margin-right: 30px;">
+                    <div class="testimonial-wrapper">
+                    <div class="testimonial-author d-flex justify-content-center">
+                        <img src="img/avatar/avatar-08.jpg" alt="...">
+                    </div>
+                    <div class="testimonial-text">
+                        <p class="font-weight-500 display-29 display-lg-28">Thanks for the great service. You won't regret it. I use Life coach often. Life coach is the most valuable business resource we have EVER purchased courses. I use Life coach often.</p>
+                    </div>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="author-details">
+                        <h4 class="h6 text-primary">Kyla Turner</h4>
+                        <div class="rating">
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                            <span><i class="fas fa-star"></i></span>
+                        </div>
+                        <p class="mb-0">Electronics Warfare</p>
+                        </div>
+                        <div class="testimonial-quote">
+                        <i class="fas fa-quote-right"></i>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+    <!-- CARROUSEL -->
+
+        
   </div>
 </template>
+<style>
+  #bannerabout {
+    background-image: url('../assets/img1.jpg');
+
+  }
+  #conteo {
+    background-image: url('../assets/img4.jpg');
+
+  }
+</style>
