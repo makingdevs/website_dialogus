df0cf955d680399b79e8a80a1733a6c1bdf9140b
Jovany Vergara <78619347+Jovany-Vergara@users.noreply.github.com>
2021-06-22 13:36:22 -0500

    #38 se le agrego el scroll

--- src/views/Noticias.vue
+++ src/views/Noticias.vue
@@ -211,3 +211,12 @@
     border-radius: 0;
 }
 </style>
+<script>
+export default{
+    created: function () {
+      window.addEventListener('scroll', this.handleScroll);
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    },
+};
+</script>
