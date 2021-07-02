cb9d418aa6c367a3bbfda964df8a6424e2d43484
Brandon Vergara <brandon@makingdevs.com>
2021-07-02 14:22:58 -0500

    [68] removing color from tag a

--- src/App.vue
+++ src/App.vue
@@ -212,11 +212,9 @@
 
 <style>
   #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
-    color: #eb2020;
   }
 
   #nav {
