71cd076d23621f8077665fdddeab173dd9317740
Jose Juan Reyes <neodevelop@gmail.com>
2021-07-08 19:10:43 -0500

    #79 Use Calendly component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -100,17 +100,17 @@
     </section>
     <!--END ABOUTUS =============================================== -->
 
+    <!-- Calendly
+        ================================================== -->
     <section>
       <div class="container">
         <div class="row">
-          <!-- Principio del widget de globo de Calendly -->
-          <link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet">
-          <script src="https://assets.calendly.com/assets/external/widget.js" type="text/javascript" async></script>
-          <script type="text/javascript">window.onload = function() { Calendly.initBadgeWidget({ url: 'https://calendly.com/makingdevs/30min', text: 'Programe una reunión conmigo', color: '#006bff', textColor: '#ffffff', branding: true }); }</script>
-          <!-- Final del widget de globo de Calendly -->
+          <vue-calendly url="https://calendly.com/makingdevs" :height="600"></vue-calendly>
         </div>
       </div>
     </section>
+    <!-- End Calendly
+        ================================================== -->
 
     <!-- ONLINE COURSES
         ================================================== -->
