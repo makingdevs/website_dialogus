838e939b43994e94023dd38ad429178698683fc6
Jovany Vergara <78619347+Jovany-Vergara@users.noreply.github.com>
2021-07-15 15:35:26 -0500

    #90 add the link by textAdministration

--- src/router/index.js
+++ src/router/index.js
@@ -86,6 +86,17 @@ const routes = [
     },
   },
   {
+    path: '/textAdministration',
+    name: 'TextAdministrations',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/TextAdministrations.vue'),
+    meta: {
+      autentificado: true
+    }
+  },
+  {
     path: '/noticias',
     name: 'Noticias',
     // route level code-splitting
