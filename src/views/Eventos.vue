f06dddfdf36e813b9b09eb8b83dca6036d6d4aa7
Jovany Vergara <78619347+Jovany-Vergara@users.noreply.github.com>
2021-07-16 13:04:10 -0500

    #92 created of page events

---
+++ src/views/Eventos.vue
@@ -0,0 +1,219 @@
+<template>
+    <div class="eventos"> 
+        <!-- PAGE TITLE
+        ================================================== -->
+        <section class="parallax page-title-section top-position bg-img cover-background" id="bannerEventos">
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-8">
+                  <h1 align="left">Eventos</h1><br>
+                  <h3 align="left" class="text-white">Contantamos con una red de expertos</h3>
+                  <h3 align="left" class=" text-warning">inscribete a nuestros eventos y lee nuestros blog</h3>
+                  <br>
+                </div>
+                <div class="col-lg-4">
+                  <img src="../assets/js/services/Estrategia/Estrategia  TT.svg" alt=""  align="left">
+                </div>
+              </div>
+            </div>
+          </section>
+
+        <!-- BLOG GRID
+        ================================================== -->
+        <section class="blog">
+            <div class="container">
+                    <h1 align="center"><i class="fas fa-angle-double-right text-warning"></i>
+                        <span class="color_34">Proximos Eventos </span></h1> <br>
+                <div class="row">
+                    <div class="col-lg-6 col-xl-4 mb-1-6 mb-md-1-9">
+                        <article class="card card-style2 card-style4  h-100">
+                            <div class="card-img">
+                                <img src="../assets/persona2.webp" alt="...">
+                            </div>
+                            <div class="card-body">
+                                <ul class="meta-tags">
+                                    <li>
+                                        <router-link to="/eventosDetalles" class="text-dark">
+                                            <i class="ti-calendar me-1 text-warning"></i> Feb 19, 2021
+                                        </router-link>
+                                    </li>
+                                </ul>
+                                <h3 class="h4 mb-3">5 Facts about life coach that will make you think twice.</h3>
+                                <p  align="left" class="text-dark">The purpose of the masterclass is to introduce the key thoughts supporting the psychodynamic approach.</p>
+
+                            </div>
+                        </article>
+                    </div>
+                    <div class="col-lg-6 col-xl-4 mb-1-6 mb-md-1-9 ">
+                        <article class="card card-style2 card-style4 h-100 ">
+                            <div class="card-img">
+                                <img src="../assets/persona4.webp" alt="">
+                            </div>
+                            <div class="card-body">
+                                <ul class="meta-tags">
+                                    <li>
+                                        <router-link to="/eventosDetalles" class="text-dark">
+                                            <i class="ti-calendar me-1 text-warning"></i> Feb 09, 2021
+                                        </router-link>
+                                    </li>
+                                </ul>
+                                <h3 class="h4 mb-3">20 Wonderful life coach. Number 16 is absolutely stunning!</h3>
+                                <p align="left" class="text-dark">The purpose of the masterclass is to introduce the key thoughts supporting the psychodynamic approach.</p>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="col-lg-6 col-xl-4 mb-1-6 mb-md-1-9">
+                        <article class="card card-style2 card-style4 h-100">
+                            <div class="card-img">
+                                <img src="../assets/persona3.webp" alt="...">
+                            </div>
+                            <div class="card-body">
+                                <ul class="meta-tags">
+                                    <li>
+                                        <router-link to="#!" class="text-dark">
+                                            <i class="ti-calendar me-1 text-warning"></i> Jan 24, 2021
+                                        </router-link>
+                                    </li>
+
+                                </ul>
+                                <h3 class="h4 mb-3">How to have a fantastic life coach with minimal spending?</h3>
+                                <p  align="left" class="text-dark">The purpose of the masterclass is to introduce the key thoughts supporting the psychodynamic approach.</p>
+                            </div>
+                        </article>
+                    </div>
+                            <!-- Event GRID
+        ================================================== -->
+                
+                    <h1 align="center"><i class="fas fa-angle-double-right text-warning"></i>
+                        <span class="color_34">Conoce Nuestro Blog</span></h1>
+                        <br><br><br>
+                    <div class="col-lg-6 col-xl-4 mb-1-6 mb-md-1-9">
+                        <article class="card card-style2 card-style4 h-100">
+                            <div class="card-img">
+                                <img src="../assets/persona5.webp" alt="...">
+                            </div>
+                            <div class="card-body">
+                                <ul class="meta-tags">
+                                    <li>
+                                        <router-link to="#!" class="text-dark">
+                                            <i class="ti-calendar me-1 text-warning"></i> Jan 24, 2021
+                                        </router-link>
+                                    </li>
+
+                                </ul>
+                                <h3 class="h4 mb-3">How to have a fantastic life coach with minimal spending?</h3>
+                                <p  align="left" class="text-dark">The purpose of the masterclass is to introduce the key thoughts supporting the psychodynamic approach.</p>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="col-lg-6 col-xl-4 mb-1-6 mb-md-1-9">
+                        <article class="card card-style2 card-style4 h-100">
+                            <div class="card-img">
+                                <img src="../assets/persona2.webp" alt="...">
+                            </div>
+                            <div class="card-body">
+                                <ul class="meta-tags">
+                                    <li>
+                                        <router-link to="#!" class="text-dark">
+                                            <i class="ti-calendar me-1 text-warning"></i> Jan 24, 2021
+                                        </router-link>
+                                    </li>
+
+                                </ul>
+                                <h3 class="h4 mb-3">How to have a fantastic life coach with minimal spending?</h3>
+                                <p  align="left" class="text-dark">The purpose of the masterclass is to introduce the key thoughts supporting the psychodynamic approach.</p>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="col-lg-6 col-xl-4 mb-1-6 mb-md-1-9">
+                        <article class="card card-style2 card-style4 h-100">
+                            <div class="card-img">
+                                <img src="../assets/persona4.webp" alt="...">
+                            </div>
+                            <div class="card-body">
+                                <ul class="meta-tags">
+                                    <li>
+                                        <router-link to="#!" class="text-dark">
+                                            <i class="ti-calendar me-1 text-warning"></i> Jan 24, 2021
+                                        </router-link>
+                                    </li>
+
+                                </ul>
+                                <h3 class="h4 mb-3">How to have a fantastic life coach with minimal spending?</h3>
+                                <p  align="left" class="text-dark">The purpose of the masterclass is to introduce the key thoughts supporting the psychodynamic approach.</p>
+                            </div>
+                        </article>
+                    </div>
+                
+
+                </div>
+
+            </div>
+        </section>
+
+    </div>
+</template>
+
+<style>
+/*PART OF EVENTS TITLES*/
+#bannerEventos {
+    background-color: #35547c;
+  }
+/*END*/
+.pagination .active a {
+    background-color: #0098a5;
+    color: #fcfcfc;
+    border: 1px solid #dbdbdb;
+    cursor: default;
+}
+
+
+.pagination a {
+    float: left;
+    font-size: 16px;
+    padding: 0 18px;
+    line-height: 40px;
+    text-decoration: none;
+    font-weight: 800;
+    border: 1px solid #ffffff;
+    background: #0098a5;
+    border-radius: 35px;
+    color: #ffffff;
+}
+.pagination a:hover {
+      background-color: #ffffff;
+      border: 1px solid #0098a5;
+      color: #0098a5; 
+      border-radius: 35px;
+      }
+      
+    #banner2 {
+        background-image: url('../assets/fondo5.jpeg');
+    }
+.tamañoCuadros{
+    object-fit: cover;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    overflow: hidden;
+    border-radius: 0;
+}
+.card-style2 .card-body {
+    padding: 40px 30px;
+    position: relative;
+    background: #fff !important;
+}
+
+</style>
+<script>
+export default{
+    created: function () {
+      window.addEventListener('scroll', this.handleScroll);
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    },
+};
+</script>
