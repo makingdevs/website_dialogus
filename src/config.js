ef64ad24b8ca50cb380e5eba93727895b6338dee
Jovany Vergara <jovanyvergara@iMac-de-Jovany.local>
2021-08-11 18:08:04 -0500

    add firebase of dialogus

--- src/config.js
+++ src/config.js
@@ -7,4 +7,13 @@ export default {
     messagingSenderId: "547238720414",
     appId: "1:547238720414:web:bff6767d6843a27fc4dc8f",
     measurementId: "G-1XZL40174D"
+
+    //apiKey: "AIzaSyCkGlVH4ZhH3xMPLY1HbU6HNWccyfE93-8",
+    //authDomain: "test-de-prueba-ff001.firebaseapp.com",
+    //databaseURL: "https://test-de-prueba-ff001-default-rtdb.firebaseio.com",
+    //projectId: "test-de-prueba-ff001",
+    //storageBucket: "test-de-prueba-ff001.appspot.com",
+    //messagingSenderId: "830645513338",
+    //appId: "1:830645513338:web:fa2471290bac5457ed1fc3",
+    //measurementId: "G-VJ356GM7QH"
   }
