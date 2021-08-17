3496384186dfed1f529925a013c6035405a537b7
Jovany Vergara <jovanyvergara@iMac-de-Jovany.local>
2021-08-17 14:42:22 -0500

    #141 change the firebase

--- src/config.js
+++ src/config.js
@@ -1,19 +1,19 @@
 export default {
-    apiKey: "AIzaSyAkQYA18BDzzx8yIXEg9YUp3jrXnOSlrFk",
-    authDomain: "website-a18fc.firebaseapp.com",
-    databaseURL: "https://website-a18fc-default-rtdb.firebaseio.com",
-    projectId: "website-a18fc",
-    storageBucket: "website-a18fc.appspot.com",
-    messagingSenderId: "547238720414",
-    appId: "1:547238720414:web:bff6767d6843a27fc4dc8f",
-    measurementId: "G-1XZL40174D"
+    //apiKey: "AIzaSyAkQYA18BDzzx8yIXEg9YUp3jrXnOSlrFk",
+    //authDomain: "website-a18fc.firebaseapp.com",
+    //databaseURL: "https://website-a18fc-default-rtdb.firebaseio.com",
+    //projectId: "website-a18fc",
+    //storageBucket: "website-a18fc.appspot.com",
+    //messagingSenderId: "547238720414",
+    //appId: "1:547238720414:web:bff6767d6843a27fc4dc8f",
+    //measurementId: "G-1XZL40174D"
 
-    //apiKey: "AIzaSyCkGlVH4ZhH3xMPLY1HbU6HNWccyfE93-8",
-    //authDomain: "test-de-prueba-ff001.firebaseapp.com",
-    //databaseURL: "https://test-de-prueba-ff001-default-rtdb.firebaseio.com",
-    //projectId: "test-de-prueba-ff001",
-    //storageBucket: "test-de-prueba-ff001.appspot.com",
-    //messagingSenderId: "830645513338",
-    //appId: "1:830645513338:web:fa2471290bac5457ed1fc3",
-    //measurementId: "G-VJ356GM7QH"
+    apiKey: "AIzaSyCkGlVH4ZhH3xMPLY1HbU6HNWccyfE93-8",
+    authDomain: "test-de-prueba-ff001.firebaseapp.com",
+    databaseURL: "https://test-de-prueba-ff001-default-rtdb.firebaseio.com",
+    projectId: "test-de-prueba-ff001",
+    storageBucket: "test-de-prueba-ff001.appspot.com",
+    messagingSenderId: "830645513338",
+    appId: "1:830645513338:web:fa2471290bac5457ed1fc3",
+    measurementId: "G-VJ356GM7QH"
   }
