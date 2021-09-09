a433f9996266435d69f119a84a532db388e6a5f8
Jovany Vergara <jovany@makingdevs.com>
2021-09-09 17:27:32 -0500

    #149 change in loader and add a video

--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -1,6 +1,8 @@
 function hideLoader() {
     const app = document.getElementById("app")
     app.classList.remove("hide")
+    const body = document.getElementsByTagName('body')[0]
+    body.style.background = null
     const e = document.getElementById("loader-app")
     e.remove();
 }
